a231bc45a886813d0a53a4fb1d2b7f8941e542fe
gyoungjun_lee <injxgj@gmail.com>
2022-01-08 16:05:36 +0900

    [feat] 계산기 클래스로 옮기기

--- createStatementData.js
+++ createStatementData.js
@@ -1,7 +1,51 @@
+class PerformanceCalculator {
+  // 공연료 계산기 클래스
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    // amountFor() 함수의 코드를 계산기 클래스로 복사
+    let result = 0;
+    switch (
+      this.play.type // amountFor() 함수가 매개변수로 받던 정보를 계산기 필드에서 바로 얻기
+    ) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          // amountFor() 함수가 매개변수로 받던 정보를 계산기 필드에서 바로 얻기
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(this.performance).type}`);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
+    return result;
+  }
+}
+
 export default function createStatementData(invoice, plays) {
   const statementData = {};
-  statementData.customer = invoice.customer; // 고객 데이터를 중간 데이터로 옮김
-  statementData.performances = invoice.performances.map(enrichPerformance); // 공연 정보를 중간 데이터로 옮김
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
@@ -9,46 +53,32 @@ export default function createStatementData(invoice, plays) {
 
   // statementData를 정제하는 util함수들
   function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // 얕은 복사
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    ); // 공연료 계산기 생성
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.play; // playFor 대신 계산기의 play 프로퍼티 이용
+    result.amount = calculator.amount; // amountFor 대신 계산기의 amount메서드 이용
+    result.volumeCredits = calculator.volumeCredits; // volumeCreditsFor() 대신 계산기의 volumeCredits이용
     return result;
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
   function volumeCreditsFor(aPerformance) {
-    let result = 0; //
-
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
   function totalVolumeCredits() {
-    return statementData.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    return statementData.performances.reduce(
+      (total, p) => total + p.volumeCredits,
+      0
+    );
   }
   function totalAmount() {
     return statementData.performances.reduce((total, p) => total + p.amount, 0);
--- statement.js
+++ statement.js
@@ -1,7 +1,9 @@
-import plays from './plays.json' assert { type: 'json' };
-import invoice from './invoices.json' assert { type: 'json' };
+// import plays from './plays.json' assert { type: 'json' };
+// import invoice from './invoices.json' assert { type: 'json' };
+import plays from "./plays.json";
+import invoice from "./invoices.json";
 
-import createStatementData from './createStatementData.js';
+import createStatementData from "./createStatementData.js";
 
 function statement() {
   return renderPlainText(createStatementData(invoice, plays)); // 필요없어진 인수 제거
@@ -12,7 +14,9 @@ function renderPlainText(statementData) {
 
   // 청구내역을 출력합니다.
   for (let perf of statementData.performances) {
-    result += ` ${perf.play.name}: ${formatAsUSD(perf.amount)} (${perf.audience}석)\n`;
+    result += ` ${perf.play.name}: ${formatAsUSD(perf.amount)} (${
+      perf.audience
+    }석)\n`;
   }
 
   result += `총액: ${formatAsUSD(statementData.totalAmount)}\n`;
@@ -26,23 +30,27 @@ function htmlStatement() {
 
 function renderHtml(data) {
   let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
-  result += '<table>\n';
-  result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
   for (let perf of data.performances) {
     result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
     result += `<td>${formatAsUSD(perf.amount)}</td></tr>\n`;
   }
-  result += '</table>\n';
+  result += "</table>\n";
   result += `<p>총액: <em>${formatAsUSD(data.totalAmount)}</em></p>\n`;
   result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
   return result;
 }
 
 function formatAsUSD(aNumber) {
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber);
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
 }
 
-const htmlObject = document.createElement('div');
-htmlObject.innerHTML = htmlStatement();
-document.querySelector('.index').appendChild(htmlObject);
+// const htmlObject = document.createElement('div');
+// htmlObject.innerHTML = htmlStatement();
+// document.querySelector('.index').appendChild(htmlObject);
 console.log(statement());
